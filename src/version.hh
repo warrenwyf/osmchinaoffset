36bab7e46177011d39d6afaff2130cddd0115805
warrenwyf <warrenwyf@gmail.com>
2022-07-23 22:35:27 +0800

    Update version to 1.0.0

--- src/version.hh
+++ src/version.hh
@@ -2,8 +2,8 @@
 
 #include <string>
 
-const std::string version = "0.1.0";
+const std::string version = "1.0.0";
 
-const int version_major = 0;
-const int version_minor = 1;
+const int version_major = 1;
+const int version_minor = 0;
 const int version_patch = 0;
